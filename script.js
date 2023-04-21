9885d9dc629c2318f6228936d94d02d98f5c7445
JPcheco <124377479+JPcheco@users.noreply.github.com>
2023-04-21 19:21:22 -0400

    UI Update
    Added Create, Bookmark, Search, Login, and Signup pages among other things

--- script.js
+++
@@ -1,8 +0,0 @@
-const greetscreen = document.querySelector('.greetscreen');
-const logo = document.querySelector('.logo');
-
-greetscreen.addEventListener('click', () => {
-    setTimeout(() => {
-        window.location.href = "homepage.html";
-    }, 0);
-});
